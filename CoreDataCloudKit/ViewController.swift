ec0e788eed3da519e9ef2b982e9e90278cd2cc0a
Luke Allen <luketimallen@gmail.com>
2019-12-13 08:36:08 -0600

    Initial Commit

---
+++ CoreDataCloudKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataCloudKit
+//
+//  Created by Luke Allen on 12/13/19.
+//  Copyright © 2019 Luke Allen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
